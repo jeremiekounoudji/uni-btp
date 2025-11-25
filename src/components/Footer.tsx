92c390af6049bbf57a40b1903dd930c9ecb0954e
Jeremie <kounoudjijeremie@gmail.com>
2025-11-25 14:41:14 +0100

    change copy rigth

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -75,7 +75,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          © Conçu et développé par RETEL CONSULTING & AFRIKA NEW TENDENCY.
+          © Conçu et développé par RETEL CONSULTING.
         </div>
       </div>
     </footer>
